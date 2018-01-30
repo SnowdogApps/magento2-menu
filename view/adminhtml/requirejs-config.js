69224ddb3de80ff862ab0918406b961b78c5249b
talalus <talal.suhail@snow.dog>
2018-01-30 03:18:49 +0200

    #21: remove old js

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -1,20 +1,3 @@
-/**
- * Copyright © 2016 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
-
 var config = {
-    map: {
-        '*': {
-            snowMenuAutoCompleteField: "Snowdog_Menu/js/auto_complete_field",
-            snowMenuEditorAdd: "Snowdog_Menu/js/add",
-            snowMenuEditorInit: "Snowdog_Menu/js/init",
-            snowMenuEditorRename: "Snowdog_Menu/js/rename",
-            snowMenuEditorSerialize: "Snowdog_Menu/js/serialize",
-            snowMenuSimpleField: "Snowdog_Menu/js/simple_field",
-            snowMenuTree: "Snowdog_Menu/js/jstree",
-            snowWysiwygSetup: "Snowdog_Menu/js/wysiwyg/setup",
-            snowMenuVue: "Snowdog_Menu/js/vue/dist/snowdog-menu"
-        }
-    }
+
 };
