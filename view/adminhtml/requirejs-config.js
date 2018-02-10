acd7f7f5ed37dc96b61b1a9d50d7ac9079c50faf
talalus <talal.suhail@snow.dog>
2018-02-10 23:45:44 +0200

    #21: switch vue to production mode

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -1,6 +1,6 @@
 var config = {
     paths: {
-        "Vue": "Snowdog_Menu/js/lib/vue",
+        "Vue": "Snowdog_Menu/js/lib/vue.min",
         "vue": "Snowdog_Menu/js/lib/require-vuejs-1.0.9",
         "Vddl": "Snowdog_Menu/js/lib/vddl",
         "vue-select": "Snowdog_Menu/js/lib/vue-select-2.4.0.min"
