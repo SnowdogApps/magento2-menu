ecd164bf01bf2b353b560d46f90c75abb15e5cf5
talalus <talal.suhail@snow.dog>
2018-03-16 22:11:34 +0200

    #21: remove version from file name

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -1,11 +1,11 @@
 var config = {
     paths: {
-        "Vue"       : "Snowdog_Menu/js/lib/vue.min",
-        "vue"       : "Snowdog_Menu/js/lib/require-vuejs-1.0.9",
-        "Vddl"      : "Snowdog_Menu/js/lib/vddl",
-        "vue-select": "Snowdog_Menu/js/lib/vue-select-2.4.0.min"
+        "Vue": "Snowdog_Menu/js/lib/vue",
+        "vue": "Snowdog_Menu/js/lib/require-vuejs",
+        "Vddl": "Snowdog_Menu/js/lib/vddl",
+        "vue-select": "Snowdog_Menu/js/lib/vue-select"
     },
-    shim : {
+    shim: {
         "Vue": { "exports": "Vue" }
     }
 };
