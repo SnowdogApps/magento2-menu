522aa2d76878c6d2329de612f895adcd140a0cb9
Lukasz Bajsarowicz <lukasz.bajsarowicz@gmail.com>
2020-07-24 15:28:50 +0200

    Feature: Index controller optimized

--- Controller/Adminhtml/Menu/Index.php
+++ Controller/Adminhtml/Menu/Index.php
@@ -1,29 +1,19 @@
 <?php
+declare(strict_types=1);
+
 namespace Snowdog\Menu\Controller\Adminhtml\Menu;
 
 use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
+use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\View\Result\PageFactory;
 
 class Index extends Action
 {
-    protected $resultPageFactory;
-
-    public function __construct(
-        Context $context,
-        PageFactory $resultPageFactory
-    ) {
-        $this->resultPageFactory = $resultPageFactory;
-        return parent::__construct($context);
-    }
+    public const ADMIN_RESOURCE = 'Snowdog_Menu::menus';
 
     public function execute()
     {
-        return $this->resultPageFactory->create();
-    }
-
-    protected function _isAllowed()
-    {
-        return $this->_authorization->isAllowed('Snowdog_Menu::menus');
+        return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
