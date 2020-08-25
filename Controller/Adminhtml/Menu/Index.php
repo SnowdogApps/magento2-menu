0f7d1d0387b17045b29b08728c796a68b85c3c15
Lukasz Bajsarowicz <lukasz.bajsarowicz@gmail.com>
2020-08-25 21:36:12 +0200

    Refactore: Remove unused imports

--- Controller/Adminhtml/Menu/Index.php
+++ Controller/Adminhtml/Menu/Index.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 namespace Snowdog\Menu\Controller\Adminhtml\Menu;
 
 use Magento\Backend\App\Action;
-use Magento\Backend\App\Action\Context;
 use Magento\Framework\Controller\ResultFactory;
-use Magento\Framework\View\Result\PageFactory;
 
 class Index extends Action
 {
     public const ADMIN_RESOURCE = 'Snowdog_Menu::menus';
 
+    /**
+     * @inheritDoc
+     */
     public function execute()
     {
         return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
