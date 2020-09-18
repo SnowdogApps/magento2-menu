f82e6b962332bb02a1dbb9a71b8e62e80886c778
Akai <akai-z@users.noreply.github.com>
2020-09-19 01:48:22 +0300

    [70191] Add page cache invalidation plugin after menu save

---
+++ Plugin/Model/ResourceModel/Menu/Save.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Snowdog\Menu\Plugin\Model\ResourceModel\Menu;
+
+use Magento\Framework\Model\AbstractModel;
+use Snowdog\Menu\Model\Menu\Cache as MenuCache;
+
+class Save
+{
+    /**
+     * @var MenuCache
+     */
+    private $menuCache;
+
+    public function __construct(MenuCache $menuCache)
+    {
+        $this->menuCache = $menuCache;
+    }
+
+    /**
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     * @return \Magento\Framework\Model\ResourceModel\Db\AbstractDb
+     */
+    public function saveAndInvalidatePageCache(AbstractModel $model, callable $proceed)
+    {
+        $isPageCacheClearable = $model->hasDataChanges();
+        $result = $proceed($model);
+
+        $this->menuCache->invalidatePageCache($isPageCacheClearable);
+
+        return $result;
+    }
+}
