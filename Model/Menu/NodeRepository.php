fbc71900400356e7fa578082f0f9778320e1c32c
Akai <akai-z@users.noreply.github.com>
2020-09-19 01:43:17 +0300

    [70191] Remove page cache invalidation from menu node repository delete method

--- Model/Menu/NodeRepository.php
+++ Model/Menu/NodeRepository.php
@@ -78,7 +78,6 @@ class NodeRepository implements NodeRepositoryInterface
     {
         try {
             $object->delete();
-            $this->cache->invalidatePageCache();
         } catch (Exception $exception) {
             throw new CouldNotDeleteException(__($exception->getMessage()));
         }
