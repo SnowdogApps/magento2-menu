bb4ef6c5054a14707445d2e21e581e83f01a5547
Samir Hajiyev <samir.hajiyev@snow.dog>
2020-09-29 14:55:13 +0400

    DEV-70196 added import nodes from categories tree

--- Ui/DataProvider/Menu/Form/MenuDataProvider.php
+++ Ui/DataProvider/Menu/Form/MenuDataProvider.php
@@ -6,6 +6,7 @@ namespace Snowdog\Menu\Ui\DataProvider\Menu\Form;
 
 use Magento\Ui\DataProvider\AbstractDataProvider;
 use Snowdog\Menu\Model\ResourceModel\Menu\CollectionFactory;
+use Magento\Ui\DataProvider\Modifier\PoolInterface;
 use Snowdog\Menu\Model\Menu;
 
 class MenuDataProvider extends AbstractDataProvider
@@ -13,15 +14,22 @@ class MenuDataProvider extends AbstractDataProvider
     /** @var array */
     private $loadedData = [];
 
+    /**
+     * @var PoolInterface
+     */
+    private $pool;
+
     public function __construct(
         $name,
         $primaryFieldName,
         $requestFieldName,
         CollectionFactory $collectionFactory,
+        PoolInterface $pool,
         array $meta = [],
         array $data = []
     ) {
         $this->collection = $collectionFactory->create();
+        $this->pool = $pool;
         parent::__construct(
             $name,
             $primaryFieldName,
@@ -31,7 +39,7 @@ class MenuDataProvider extends AbstractDataProvider
         );
     }
 
-    public function getData(): array
+    public function _getData(): array
     {
         if (!empty($this->loadedData)) {
             return $this->loadedData;
@@ -45,5 +53,34 @@ class MenuDataProvider extends AbstractDataProvider
         }
 
         return $this->loadedData;
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getData()
+    {
+        /** @var ModifierInterface $modifier */
+        foreach ($this->pool->getModifiersInstances() as $modifier) {
+            $this->data = $modifier->modifyData($this->data);
+        }
+
+        return $this->data;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMeta()
+    {
+        $meta = parent::getMeta();
+
+        /** @var ModifierInterface $modifier */
+        foreach ($this->pool->getModifiersInstances() as $modifier) {
+            $meta = $modifier->modifyMeta($meta);
+        }
+
+        return $meta;
     }
 }
