4e98faecea53ee5dbc40ebc711dbaa8cdbda9a95
Janusz Janczy <janusz.janczy@snow.dog>
2020-09-30 22:03:23 -0300

    #69072: possible way of loading additional components defined via php method

--- view/adminhtml/web/js/nodes.js
+++ view/adminhtml/web/js/nodes.js
@@ -5,22 +5,26 @@ define([
     "vue!Snowdog_Menu/vue/app",
     "vue!Snowdog_Menu/vue/nested-list",
     "vue!Snowdog_Menu/vue/menu-type",
-    "vue!Snowdog_Menu/vue/menu-type/category",
-    "vue!Snowdog_Menu/vue/menu-type/cms-page",
-    "vue!Snowdog_Menu/vue/menu-type/cms-block",
-    "vue!Snowdog_Menu/vue/menu-type/custom-url",
-    "vue!Snowdog_Menu/vue/menu-type/product",
-    "vue!Snowdog_Menu/vue/menu-type/wrapper",
     "vue!Snowdog_Menu/vue/field-type/simple",
     "vue!Snowdog_Menu/vue/field-type/checkbox",
     "vue!Snowdog_Menu/vue/field-type/autocomplete"
 ], function(Vue, Vddl, vueSelect) {
+
     return function(config) {
-        Vue.use(Vddl);
-        Vue.component('v-select', vueSelect.VueSelect);
-        var app = new Vue({
-            el  : config.el || "#snowdog_menu",
-            data: config.data
+        var dependencies = [];
+
+        if (config.vueComponents && config.vueComponents.length > 0) {
+            dependencies = config.vueComponents;
+        }
+
+        require(dependencies, function() {
+            Vue.use(Vddl);
+
+            Vue.component('v-select', vueSelect.VueSelect);
+            var app = new Vue({
+                el  : config.el || "#snowdog_menu",
+                data: config.data
+            });
         });
     }
 });
