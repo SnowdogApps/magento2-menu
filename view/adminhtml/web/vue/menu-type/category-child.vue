10859bb1f5041c16ed8e30d927d10d842ce995d7
Kamil Balwierz <kamil@snow.dog>
2020-10-12 19:18:40 +0200

    Autofixing eslint

--- view/adminhtml/web/vue/menu-type/category-child.vue
+++ view/adminhtml/web/vue/menu-type/category-child.vue
@@ -1,20 +1,19 @@
 <template>
     <div class="admin__field field">
         <auto-complete
-                :label="config.translation.categoryChild"
-                :description="config.translation.categoryId"
-                :item="item"
-                :options="config.fieldData.category_child.snowMenuAutoCompleteField.options"
-                :config="config"
-        >
-        </auto-complete>
+            :label="config.translation.categoryChild"
+            :description="config.translation.categoryId"
+            :item="item"
+            :options="config.fieldData.category_child.snowMenuAutoCompleteField.options"
+            :config="config"
+        />
     </div>
 </template>
 <script>
     define(["Vue"], function(Vue) {
         Vue.component("category_child", {
-            template: template,
-            props: ['config', 'item']
+            props: ['config', 'item'],
+            template: template
         });
     });
 </script>
