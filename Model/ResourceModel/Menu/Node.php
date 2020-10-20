3b10e117b50788dc071dca27fc443e8668f3eb36
Akai <akai-z@users.noreply.github.com>
2020-10-21 01:08:11 +0300

    [70197] Add a menu node resource model method that bulk inserts menu records

--- Model/ResourceModel/Menu/Node.php
+++ Model/ResourceModel/Menu/Node.php
@@ -5,8 +5,33 @@ use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
 
 class Node extends AbstractDb
 {
+    const BULK_INSERT_BATCH_SIZE = 500;
+
     protected function _construct()
     {
         $this->_init('snowmenu_node', 'node_id');
+    }
+
+    /**
+     * @param array $nodes
+     * @throws Exception
+     */
+    public function insertMultiple(array $nodes)
+    {
+        $connection = $this->getConnection();
+        $table = $this->getMainTable();
+
+        $connection->beginTransaction();
+
+        try {
+            foreach (array_chunk($nodes, self::BULK_INSERT_BATCH_SIZE) as $chunk) {
+                $connection->insertMultiple($table, $chunk);
+            }
+
+            $connection->commit();
+        } catch (\Exception $exception) {
+            $connection->rollBack();
+            throw $exception;
+        }
     }
 }
