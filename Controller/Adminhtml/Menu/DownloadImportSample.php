f75c9c80c21c224c428f816d7ff259deb936a93d
Akai <akai-z@users.noreply.github.com>
2020-10-26 02:17:21 +0300

    [70197] Add menu admin controller import sample file download action

---
+++ Controller/Adminhtml/Menu/DownloadImportSample.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Snowdog\Menu\Controller\Adminhtml\Menu;
+
+use Magento\Backend\App\Action;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\App\Response\Http\FileFactory as HttpFileFactory;
+use Psr\Log\LoggerInterface;
+use Snowdog\Menu\Model\Menu\Import\SampleFile as ImportSampleFile;
+
+class DownloadImportSample extends Action
+{
+    /**
+     * @inheritDoc
+     */
+    const ADMIN_RESOURCE = 'Snowdog_Menu::menus';
+
+    /**
+     * @var HttpFileFactory
+     */
+    private $httpFileFactory;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var ImportSampleFile
+     */
+    private $importSampleFile;
+
+    public function __construct(
+        Action\Context $context,
+        HttpFileFactory $httpFileFactory,
+        LoggerInterface $logger,
+        ImportSampleFile $importSampleFile
+    ) {
+        $this->httpFileFactory = $httpFileFactory;
+        $this->logger = $logger;
+        $this->importSampleFile = $importSampleFile;
+
+        parent::__construct($context);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function execute()
+    {
+        try {
+            return $this->httpFileFactory->create(
+                "menu-sample.csv",
+                $this->importSampleFile->getFileDownloadContent(),
+                DirectoryList::VAR_DIR
+            );
+        } catch (\Exception $exception) {
+            $this->logger->critical($exception);
+            $this->messageManager->addErrorMessage(
+                __('An error occurred while generating menu import sample file.')
+            );
+        }
+
+        $resultRedirect = $this->resultRedirectFactory->create();
+        return $resultRedirect->setPath('*/*');
+    }
+}
