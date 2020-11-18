8a0061a6a90d27e4cdf2daac219eac76cd1dc455
Akai <akai-z@users.noreply.github.com>
2020-11-18 17:53:42 +0200

    [70197] Apply node import validator model to node import processor

--- Model/Menu/ImportProcessor/Node.php
+++ Model/Menu/ImportProcessor/Node.php
@@ -2,24 +2,13 @@
 
 namespace Snowdog\Menu\Model\Menu\ImportProcessor;
 
-use Magento\Framework\Exception\ValidatorException;
-use Magento\Framework\Serialize\SerializerInterface;
 use Snowdog\Menu\Api\Data\NodeInterface;
 use Snowdog\Menu\Api\Data\NodeInterfaceFactory;
 use Snowdog\Menu\Api\NodeRepositoryInterface;
-use Snowdog\Menu\Model\NodeTypeProvider;
+use Snowdog\Menu\Model\Menu\ExportProcessor;
 
 class Node
 {
-    const REQUIRED_FIELDS = [
-        NodeInterface::TYPE
-    ];
-
-    /**
-     * @var SerializerInterface
-     */
-    private $serializer;
-
     /**
      * @var NodeInterfaceFactory
      */
@@ -31,109 +20,63 @@ class Node
     private $nodeRepository;
 
     /**
-     * @var NodeTypeProvider
-     */
-    private $nodeTypeProvider;
-
-    /**
      * @var Node\Catalog
      */
     private $nodeCatalog;
 
+    /**
+     * @var Node\Validator
+     */
+    private $validator;
+
     public function __construct(
-        SerializerInterface $serializer,
         NodeInterfaceFactory $nodeFactory,
         NodeRepositoryInterface $nodeRepository,
-        NodeTypeProvider $nodeTypeProvider,
-        Node\Catalog $nodeCatalog
+        Node\Catalog $nodeCatalog,
+        Node\Validator $validator
     ) {
-        $this->serializer = $serializer;
         $this->nodeFactory = $nodeFactory;
         $this->nodeRepository = $nodeRepository;
-        $this->nodeTypeProvider = $nodeTypeProvider;
         $this->nodeCatalog = $nodeCatalog;
+        $this->validator = $validator;
     }
 
     /**
      * @param int $menuId
+     * @param int $nodesLevel
+     * @param int|null $parentNode
      */
-    public function createNodes(array $nodes, $menuId)
+    public function createNodes(array $nodes, $menuId, $nodesLevel = 0, $parentNode = null)
     {
-        $nodesIds = [];
-
         foreach ($nodes as $nodeData) {
             $node = $this->nodeFactory->create();
-            $processedNodeData = $this->getProcessedNodeData($nodeData, $menuId, $nodesIds);
+            $data = $this->getProcessedNodeData($nodeData, $menuId, $nodesLevel, $parentNode);
 
-            $node->setData($processedNodeData);
+            $node->setData($data);
             $this->nodeRepository->save($node);
 
-            if (isset($nodeData[NodeInterface::NODE_ID])) {
-                $nodesIds[$nodeData[NodeInterface::NODE_ID]] = $node->getId();
+            if (isset($nodes[ExportProcessor::NODES_FIELD])) {
+                $this->createNodes($nodes[ExportProcessor::NODES_FIELD], $menuId, $nodesLevel + 1, $node->getId());
             }
         }
     }
 
-    /**
-     * @throws ValidatorException
-     */
     public function validateImportData(array $data)
     {
-        $nodeTypes = array_keys($this->nodeTypeProvider->getLabels());
-
-        foreach ($data as $nodeNumber => $node) {
-            $missingFields = [];
-            foreach (self::REQUIRED_FIELDS as $field) {
-                if (empty($node[$field])) {
-                    $missingFields[] = $field;
-                }
-            }
-
-            if ($missingFields) {
-                throw new ValidatorException(
-                    __(
-                        'The following node "%1" required import fields are missing: "%2".',
-                        $nodeNumber + 1,
-                        implode('", "', $missingFields)
-                    )
-                );
-            }
-
-            if (!in_array($node[NodeInterface::TYPE], $nodeTypes)) {
-                throw new ValidatorException(__('Node "%1" type is invalid.', $nodeNumber + 1));
-            }
-        }
-    }
-
-    /**
-     * @param string $data
-     * @throws ValidatorException
-     * @return array
-     */
-    public function getNodesJsonData($data)
-    {
-        try {
-            return $this->serializer->unserialize($data);
-        } catch (\InvalidArgumentException $exception) {
-            throw new ValidatorException(__('Invalid menu nodes JSON format.'));
-        }
+        $this->validator->validate($data);
     }
 
     /**
      * @param int $menuId
+     * @param int $nodesLevel
+     * @param int|null $parentId
      * @return array
      */
-    private function getProcessedNodeData(array $data, $menuId, array $nodesIds)
+    private function getProcessedNodeData(array $data, $menuId, $nodesLevel = 0, $parentId = null)
     {
         $data[NodeInterface::MENU_ID] = $menuId;
-
-        if (isset($data[NodeInterface::PARENT_ID])) {
-            $data[NodeInterface::PARENT_ID] = $nodesIds[$data[NodeInterface::PARENT_ID]] ?? null;
-        }
-
-        if (empty($data[NodeInterface::PARENT_ID])) {
-            $data[NodeInterface::LEVEL] = 0;
-        }
+        $data[NodeInterface::PARENT_ID] = $parentId;
+        $data[NodeInterface::LEVEL] = $nodesLevel;
 
         switch ($data[NodeInterface::TYPE]) {
             case Node\Catalog::PRODUCT_NODE_TYPE:
@@ -150,8 +93,6 @@ class Node
         if (isset($data[NodeInterface::IS_ACTIVE])) {
             $data[NodeInterface::IS_ACTIVE] = (bool) $data[NodeInterface::IS_ACTIVE];
         }
-
-        unset($data[NodeInterface::NODE_ID]);
 
         return $data;
     }
