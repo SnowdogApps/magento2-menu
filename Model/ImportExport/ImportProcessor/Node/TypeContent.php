e6c0997417910e55a05271b83e0d59e5783d8315
Akai <akai-z@users.noreply.github.com>
2020-11-23 21:53:35 +0200

    [70197] Add node import processor type content model

---
+++ Model/ImportExport/ImportProcessor/Node/TypeContent.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Snowdog\Menu\Model\ImportExport\ImportProcessor\Node;
+
+use Snowdog\Menu\Model\ImportExport\ImportProcessor\Node\Type\Catalog;
+use Snowdog\Menu\Model\ImportExport\ImportProcessor\Node\Type\Cms;
+
+class TypeContent
+{
+    /**
+     * @var Catalog
+     */
+    private $catalog;
+
+    /**
+     * @var Cms
+     */
+    private $cms;
+
+    public function __construct(Catalog $catalog, Cms $cms)
+    {
+        $this->catalog = $catalog;
+        $this->cms = $cms;
+    }
+
+    /**
+     * @param string $type
+     * @param mixed $content
+     * @return mixed
+     */
+    public function get($type, $content)
+    {
+        switch ($type) {
+            case Catalog::PRODUCT_NODE_TYPE:
+                $product = $this->catalog->getProduct($content);
+                $content = $product->getId();
+
+                break;
+            case Cms::BLOCK_NODE_TYPE:
+                $block = $this->cms->getBlock($content);
+                $content = $block->getIdentifier();
+
+                break;
+            case Cms::PAGE_NODE_TYPE:
+                $page = $this->cms->getPage($content);
+                $content = $page->getIdentifier();
+
+                break;
+        }
+
+        return $content;
+    }
+}
