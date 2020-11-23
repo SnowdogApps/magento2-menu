5f93cda63cd4fc193ea7c66aa04c773f0a0c6e0a
Akai <akai-z@users.noreply.github.com>
2020-11-23 21:52:47 +0200

    [70197] Add menu import processor identifier model

---
+++ Model/ImportExport/ImportProcessor/Menu/Identifier.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Snowdog\Menu\Model\ImportExport\ImportProcessor\Menu;
+
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Snowdog\Menu\Api\Data\MenuInterface;
+use Snowdog\Menu\Api\MenuRepositoryInterface;
+
+class Identifier
+{
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    private $searchCriteriaBuilder;
+
+    /**
+     * @var MenuRepositoryInterface
+     */
+    private $menuRepository;
+
+    public function __construct(
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        MenuRepositoryInterface $menuRepository
+    ) {
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->menuRepository = $menuRepository;
+    }
+
+    /**
+     * @param string $identifier
+     * @return string
+     */
+    public function getNewIdentifier($identifier)
+    {
+        $menus = $this->getMenuListByIdentifier($identifier);
+        if (!$menus) {
+            return $identifier;
+        }
+
+        $identifiers = [];
+        foreach ($menus as $menu) {
+            $identifiers[$menu->getIdentifier()] = $menu->getId();
+        }
+
+        $idNumber = 1;
+        $newIdentifier = $identifier . '-' . $idNumber;
+
+        while (isset($identifiers[$newIdentifier])) {
+            $newIdentifier = $identifier . '-' . (++$idNumber);
+        }
+
+        return $newIdentifier;
+    }
+
+    /**
+     * @param string $identifier
+     * @return array
+     */
+    private function getMenuListByIdentifier($identifier)
+    {
+        $searchCriteria = $this->searchCriteriaBuilder
+            ->addFilter(MenuInterface::IDENTIFIER, "${identifier}%", 'like')
+            ->create();
+
+        return $this->menuRepository->getList($searchCriteria)->getItems();
+    }
+}
