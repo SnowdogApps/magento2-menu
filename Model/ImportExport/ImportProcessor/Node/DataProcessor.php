6b10e4de7d4e454d591fa9a7680db0cc91eb5a1a
Akai <akai-z@users.noreply.github.com>
2020-11-23 21:55:30 +0200

    [70197] Add node import data processor

---
+++ Model/ImportExport/ImportProcessor/Node/DataProcessor.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Snowdog\Menu\Model\ImportExport\ImportProcessor\Node;
+
+use Snowdog\Menu\Api\Data\NodeInterface;
+use Snowdog\Menu\Model\ImportExport\ExportProcessor;
+use Snowdog\Menu\Model\ImportExport\ImportProcessor\FieldProcessor\Boolean as BooleanField;
+
+class DataProcessor
+{
+    /**
+     * @var BooleanField
+     */
+    private $booleanField;
+
+    /**
+     * @var TypeContent
+     */
+    private $typeContent;
+
+    public function __construct(BooleanField $booleanField, TypeContent $typeContent)
+    {
+        $this->booleanField = $booleanField;
+        $this->typeContent = $typeContent;
+    }
+
+    /**
+     * @param int $menuId
+     * @param int $nodesLevel
+     * @param int|null $parentId
+     * @return array
+     */
+    public function get(array $data, $menuId, $nodesLevel = 0, $parentId = null)
+    {
+        $data[NodeInterface::MENU_ID] = $menuId;
+        $data[NodeInterface::PARENT_ID] = $parentId;
+        $data[NodeInterface::LEVEL] = $nodesLevel;
+
+        $data[NodeInterface::CONTENT] = $this->typeContent->get(
+            $data[NodeInterface::TYPE],
+            $data[NodeInterface::CONTENT]
+        );
+
+        if (isset($data[NodeInterface::TARGET])) {
+            $data[NodeInterface::TARGET] = $this->booleanField->getValue($data[NodeInterface::TARGET]);
+        }
+
+        if (isset($data[NodeInterface::IS_ACTIVE])) {
+            $data[NodeInterface::IS_ACTIVE] = $this->booleanField->getValue($data[NodeInterface::IS_ACTIVE]);
+        }
+
+        unset($data[ExportProcessor::NODES_FIELD]);
+
+        return $data;
+    }
+}
