dcb485fd9c88a35a6db067d3c2a61c65abad7c89
Akai <akai-z@users.noreply.github.com>
2020-11-23 22:01:56 +0200

    [70197] Use menu data processor in menu import processor

--- Model/ImportExport/ImportProcessor/Menu.php
+++ Model/ImportExport/ImportProcessor/Menu.php
@@ -2,18 +2,12 @@
 
 namespace Snowdog\Menu\Model\ImportExport\ImportProcessor;
 
-use Magento\Framework\Api\SearchCriteriaBuilder;
 use Snowdog\Menu\Api\Data\MenuInterface;
 use Snowdog\Menu\Api\Data\MenuInterfaceFactory;
 use Snowdog\Menu\Api\MenuRepositoryInterface;
 
 class Menu
 {
-    /**
-     * @var SearchCriteriaBuilder
-     */
-    private $searchCriteriaBuilder;
-
     /**
      * @var MenuInterfaceFactory
      */
@@ -25,26 +19,19 @@ class Menu
     private $menuRepository;
 
     /**
-     * @var Menu\Store
-     */
-    private $store;
-
-    /**
      * @var Menu\Validator
      */
     private $validator;
 
     public function __construct(
-        SearchCriteriaBuilder $searchCriteriaBuilder,
         MenuInterfaceFactory $menuFactory,
         MenuRepositoryInterface $menuRepository,
-        Menu\Store $store,
+        Menu\DataProcessor $dataProcessor,
         Menu\Validator $validator
     ) {
-        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
         $this->menuFactory = $menuFactory;
         $this->menuRepository = $menuRepository;
-        $this->store = $store;
+        $this->dataProcessor = $dataProcessor;
         $this->validator = $validator;
     }
 
@@ -55,9 +42,9 @@ class Menu
     {
         $menu = $this->menuFactory->create();
 
-        $menu->setData($this->getProcessedMenuData($data));
+        $menu->setData($this->dataProcessor->getMenuData($data));
         $this->menuRepository->save($menu);
-        $menu->saveStores($this->getStoreIds($stores));
+        $menu->saveStores($this->dataProcessor->getStoreIds($stores));
 
         return $menu;
     }
@@ -65,72 +52,5 @@ class Menu
     public function validateImportData(array $data)
     {
         $this->validator->validate($data);
-    }
-
-    /**
-     * @param string $identifier
-     * @return string
-     */
-    private function getNewMenuIdentifier($identifier)
-    {
-        $menus = $this->getMenuListByIdentifier($identifier);
-        if (!$menus) {
-            return $identifier;
-        }
-
-        $identifiers = [];
-        foreach ($menus as $menu) {
-            $identifiers[$menu->getIdentifier()] = $menu->getId();
-        }
-
-        $idNumber = 1;
-        $newIdentifier = $identifier . '-' . $idNumber;
-
-        while (isset($identifiers[$newIdentifier])) {
-            $newIdentifier = $identifier . '-' . (++$idNumber);
-        }
-
-        return $newIdentifier;
-    }
-
-    /**
-     * @param string $identifier
-     * @return array
-     */
-    private function getMenuListByIdentifier($identifier)
-    {
-        $searchCriteria = $this->searchCriteriaBuilder
-            ->addFilter(MenuInterface::IDENTIFIER, "${identifier}%", 'like')
-            ->create();
-
-        return $this->menuRepository->getList($searchCriteria)->getItems();
-    }
-
-    /**
-     * @return array
-     */
-    private function getProcessedMenuData(array $data)
-    {
-        $data[MenuInterface::IDENTIFIER] = $this->getNewMenuIdentifier($data[MenuInterface::IDENTIFIER]);
-
-        if (isset($data[MenuInterface::IS_ACTIVE])) {
-            $data[MenuInterface::IS_ACTIVE] = (bool) $data[MenuInterface::IS_ACTIVE];
-        }
-
-        return $data;
-    }
-
-    /**
-     * @return array
-     */
-    private function getStoreIds(array $storeCodes)
-    {
-        $storeIds = [];
-
-        foreach ($storeCodes as $storeCode) {
-            $storeIds[] = $this->store->get($storeCode)->getId();
-        }
-
-        return $storeIds;
     }
 }
