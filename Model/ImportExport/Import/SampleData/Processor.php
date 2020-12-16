ff2a816155fb4b317fd6aea06061a0b8acdb5bdb
Akai <akai-z@users.noreply.github.com>
2020-12-16 13:53:21 +0200

    [70197] Use import sample data field processor in sample data main processor model

--- Model/ImportExport/Import/SampleData/Processor.php
+++ Model/ImportExport/Import/SampleData/Processor.php
@@ -4,13 +4,19 @@ declare(strict_types=1);
 
 namespace Snowdog\Menu\Model\ImportExport\Import\SampleData;
 
+use Snowdog\Menu\Model\ImportExport\Import\SampleData\Processor\Field as FieldProcessor;
+
 class Processor
 {
-    const BOOLEAN_TYPES = ['smallint', 'tinyint'];
-    const BOOLEAN_FIELD_DEFAULT_VALUE = 'Valid values: <1 | 0>';
+    /**
+     * @var FieldProcessor
+     */
+    private $fieldProcessor;
 
-    const OPTIONAL_FIELD_LABEL = '[Optional]';
-    const TIMESTAMP_DEFAULT_VALUE = 'current_timestamp()';
+    public function __construct(FieldProcessor $fieldProcessor)
+    {
+        $this->fieldProcessor = $fieldProcessor;
+    }
 
     public function getFieldsData(array $fields, array $excludedFields = [], array $defaultData = []): array
     {
@@ -19,44 +25,10 @@ class Processor
 
         foreach ($fields as $field => $fieldDescription) {
             if (!isset($excludedFields[$field])) {
-                $fieldsData[$field] = $this->getFieldData($field, $fieldDescription, $defaultData);
+                $fieldsData[$field] = $this->fieldProcessor->getData($field, $fieldDescription, $defaultData);
             }
         }
 
         return $fieldsData;
-    }
-
-    private function getFieldData(string $field, array $fieldDescription, array $defaultData = []): string
-    {
-        switch (true) {
-            case array_key_exists($field, $defaultData):
-                $data = (string) $defaultData[$field];
-                break;
-            case in_array($fieldDescription['DATA_TYPE'], self::BOOLEAN_TYPES):
-                $data = self::BOOLEAN_FIELD_DEFAULT_VALUE;
-                break;
-            default:
-                $data = '[Type: ' . $fieldDescription['DATA_TYPE'] . ']';
-        }
-
-        return $data . $this->getFieldDetails($fieldDescription);
-    }
-
-    private function getFieldDetails(array $fieldDescription): string
-    {
-        $details = [];
-
-        if ($fieldDescription['NULLABLE']) {
-            $details[] = self::OPTIONAL_FIELD_LABEL;
-        }
-
-        if (isset($fieldDescription['DEFAULT'])
-            && $fieldDescription['DEFAULT'] !== ''
-            && $fieldDescription['DEFAULT'] !== self::TIMESTAMP_DEFAULT_VALUE
-        ) {
-            $details[] = '[Default: ' . $fieldDescription['DEFAULT'] . ']';
-        }
-
-        return $details ? ' - ' . implode(' - ', $details) : '';
     }
 }
