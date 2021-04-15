35096efa137190b5a3fa5862175059be5de30bef
Emanuele Gian <emanuele.gian@snow.dog>
2021-04-15 11:29:32 +0200

    Commit two ugly files to check CS from fork

---
+++ Model/MyUglyClass.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Class MyUglyClass
+ * @package Snowdog\Menu\Model
+ * @copyright Copyright © 2021 Snowdog. All rights reserved.
+ * @see https://snow.dog
+ */
+declare(strict_types=1);
+
+
+namespace Snowdog\Menu\Model;
+
+
+class MyUglyClass
+{
+    public function MyUglyMethod($notUsedArgument) {
+        echo "Hey, This method isn't following PHP CS rules, so CS in Github should be fail!... We'll see what happen there, noooo?";
+    }
+
+}
+
+
+
+
+
+
+
