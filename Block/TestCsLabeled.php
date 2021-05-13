6231b89d8fecc638e0a80a7991277ed1a96c935e
emanuelegian <82643587+emanuelegian@users.noreply.github.com>
2021-05-13 15:35:53 +0200

    Create TestCsLabeled.php

---
+++ Block/TestCsLabeled.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Snowdog\Menu\Block;
+
+class TestCsLabeled
+{
+    public function MyUglyMethodTest($test)
+    {
+        return 'This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. ';
+    }
+
+
+}
+
+
