6bba6f644ff075786c96664f2449df3e9f2ef328
Ania <anna.karon@snow.dog>
2021-06-21 23:53:57 +0200

    79677 fix rendering templates list

--- view/adminhtml/web/vue/field-type/autocomplete.vue
+++ view/adminhtml/web/vue/field-type/autocomplete.vue
@@ -28,7 +28,7 @@
                 <template v-slot:option="option">
                     {{ option.label }}
 
-                    <template v-if="option.store.length">
+                    <template v-if="option.store && option.store.length">
                         <span class="vs__dropdown-option__details">
                             {{ option.store.join(', ') }}
                         </span>
