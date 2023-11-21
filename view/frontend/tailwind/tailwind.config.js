f47247752aacef793b01357869ed2adb8bf3ec8e
Ola <ola.frankowska@snow.dog>
2023-11-21 09:21:06 +0100

    #101754 - add initial hyva support

---
+++ view/frontend/tailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+/**
+ * Hyvä Themes - https://hyva.io
+ * Copyright © Hyvä Themes 2022-present. All rights reserved.
+ * This product is licensed per Magento install
+ * See https://hyva.io/license
+ */
+
+module.exports = {
+    content: [
+        '../templates/**/*.phtml',
+    ]
+}
