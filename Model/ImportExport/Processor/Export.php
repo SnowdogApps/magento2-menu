87c8b216556ed4ad78daf15c2b0d0bc89df0a22a
Martyna Maciejewska <martyna.maciejewska@snow.dog>
2024-02-08 09:22:02 +0100

    Get menu row_id for import and export actions

--- Model/ImportExport/Processor/Export.php
+++ Model/ImportExport/Processor/Export.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Snowdog\Menu\Model\ImportExport\Processor;
 
+use Snowdog\Menu\Helper\MenuHelper;
 use Snowdog\Menu\Model\ImportExport\Processor\Export\Menu;
 use Snowdog\Menu\Model\ImportExport\Processor\Export\Node;
 use Snowdog\Menu\Model\ImportExport\Processor\ExtendedFields;
@@ -20,16 +21,22 @@ class Export
      */
     private $node;
 
-    public function __construct(Menu $menu, Node $node)
+    /**
+     * @var MenuHelper
+     */
+    private $menuHelper;
+
+    public function __construct(Menu $menu, Node $node, MenuHelper $menuHelper)
     {
         $this->menu = $menu;
         $this->node = $node;
+        $this->menuHelper = $menuHelper;
     }
 
     public function getExportData(int $menuId): array
     {
         $data = $this->menu->getData($menuId);
-        $nodes = $this->node->getList($menuId);
+        $nodes = $this->node->getList((int) $data[$this->menuHelper->getLinkField()]);
 
         if ($nodes) {
             $data[ExtendedFields::NODES] = $nodes;
