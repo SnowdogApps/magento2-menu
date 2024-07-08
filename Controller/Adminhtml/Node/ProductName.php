4f5bf91d71396581671eab5b84196036fa77a104
Adam Wacławczyk <adam.waclawczyk@snow.dog>
2024-07-08 14:53:47 +0200

    #104362 add admin snowmenu/node/productName controller

---
+++ Controller/Adminhtml/Node/ProductName.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace Snowdog\Menu\Controller\Adminhtml\Node;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Exception\InputException;
+
+class ProductName extends Action implements HttpGetActionInterface
+{
+    /**
+     * @inheritDoc
+     */
+    const ADMIN_RESOURCE = 'Snowdog_Menu::menus';
+
+    /**
+     * @var \Magento\Catalog\Api\ProductRepositoryInterface
+     */
+    private $productRepository;
+
+    public function __construct(
+        Context $context,
+        ProductRepositoryInterface $productRepository
+    ) {
+        parent::__construct($context);
+        $this->productRepository = $productRepository;
+    }
+
+    /**
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     * @throws \Magento\Framework\Exception\InputException
+     */
+    public function execute()
+    {
+        $storeId = (int) $this->_request->getParam('store_id', 0);
+        $productId = $this->_request->getParam('product_id');
+        if (empty($productId)) {
+            throw new InputException(__("Missing required product_id param"));
+        }
+        $product = $this->productRepository->getById($productId, false, $storeId);
+        $result = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+
+        return $result->setData(['product_name' => $product->getName()]);
+    }
+}
